3891e940aeafed3fb0c289d9e609a2ce915cba55
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-05-11 15:28:27 +0900

    Added coredata with model and manager

---
+++ CoreDataProfileApplication/Sources/Models/User+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var surname: String?
+
+}
+
+extension User : Identifiable {
+
+}
