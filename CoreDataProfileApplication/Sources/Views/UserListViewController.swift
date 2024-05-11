6bd5ad9c7c250df0f9ce8c0d4c5cafb4ac19a416
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-05-11 14:22:01 +0900

    Added a simple home screen with users

---
+++ CoreDataProfileApplication/Sources/Views/UserListViewController.swift
@@ -0,0 +1,113 @@
+import UIKit
+
+class UserListViewController: UIViewController {
+
+    var model = ["CellOne", "CellTwo", "CellThree"]
+
+    // MARK: - UI
+
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = .systemGray6
+        textField.placeholder = "Print your name here"
+        textField.indent(size: 20)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Press", for: .normal)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .insetGrouped)
+        table.backgroundColor = .white
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.dataSource = self
+        table.delegate = self
+        table.layer.cornerRadius = 10
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+
+    // MARK: - Actions
+
+    @objc
+    private func buttonTapped() { }
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setups
+
+    private func setupView() {
+        view.backgroundColor = .white
+        title = "Users"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func setupHierarchy() {
+        [textField, button, tableView].forEach { view.addSubview($0) }
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            textField.heightAnchor.constraint(equalToConstant: 52),
+
+            button.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 15),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            button.heightAnchor.constraint(equalToConstant: 52),
+
+            tableView.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 15),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15)
+        ])
+    }
+}
+
+// MARK: - Data source
+
+extension UserListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        model.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+}
+
+// MARK: - Delegate
+
+extension UserListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let detailVC = UserDetailsViewController()
+        let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = backButton
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+}
