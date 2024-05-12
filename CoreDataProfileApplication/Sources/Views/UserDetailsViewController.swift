7c3f4df5263c6f4a512b3e85e48fa0382b7de7dd
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-05-12 13:39:59 +0900

    Reworked the user details view

--- CoreDataProfileApplication/Sources/Views/UserDetailsViewController.swift
+++ CoreDataProfileApplication/Sources/Views/UserDetailsViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 final class UserDetailsViewController: UIViewController {
 
+    // MARK: - Properties
+
+    private var isEditingProfile = false
+
     // MARK: - UI
 
     private lazy var buttonEdit: UIButton = {
@@ -17,30 +21,67 @@ final class UserDetailsViewController: UIViewController {
         return button
     }()
 
-    private lazy var image: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(systemName: "number")
-        image.layer.masksToBounds = true
-        image.layer.borderWidth = 1
-        image.layer.borderColor = UIColor.systemGray.cgColor
-        image.layer.cornerRadius = 125
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
-
-    private lazy var labelName: UILabel = {
+    private lazy var label: UILabel = {
         let label = UILabel()
-        label.font = .systemFont(ofSize: 20, weight: .regular)
-        label.text = "Cell"
-        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
+    }()
+
+    private lazy var textFieldName: UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = .systemGray6
+        textField.indent(size: 20)
+        textField.isUserInteractionEnabled = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    private lazy var textFieldSurname: UITextField = {
+        let textField = UITextField()
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = .systemGray6
+        textField.indent(size: 20)
+        textField.isUserInteractionEnabled = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+
+    private lazy var buttonSaved: UIButton = {
+        let button = UIButton(type: .system)
+        button.isHidden = true
+        button.setTitle("Save", for: .normal)
+        button.layer.cornerRadius = 10
+        button.backgroundColor = .systemBlue
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
 
     // MARK: - Actions
 
     @objc
-    private func editButtonTapped() { }
+    private func editButtonTapped() { 
+        if isEditingProfile {
+            buttonEdit.setTitle("Back", for: .normal)
+            updateEditingUI(isEditing: true)
+        } else {
+            buttonEdit.setTitle("Edit", for: .normal)
+            updateEditingUI(isEditing: false)
+        }
+    }
+
+    @objc
+    private func saveButtonTapped() {
+        updateEditingUI(isEditing: false)
+        if isEditingProfile {
+            buttonEdit.setTitle("Edit", for: .normal)
+        }
+    }
 
     // MARK: - Lifecycle
 
@@ -49,6 +90,7 @@ final class UserDetailsViewController: UIViewController {
         setupView()
         setupHierarchy()
         setupLayout()
+        updateEditingUI(isEditing: false)
     }
 
     // MARK: - Setups
@@ -59,7 +101,8 @@ final class UserDetailsViewController: UIViewController {
     }
 
     private func setupHierarchy() {
-        [buttonEdit, image, labelName].forEach { view.addSubview($0) }
+        [buttonEdit, label, textFieldName, textFieldSurname, buttonSaved]
+            .forEach { view.addSubview($0) }
     }
 
     private func setupLayout() {
@@ -67,14 +110,33 @@ final class UserDetailsViewController: UIViewController {
             buttonEdit.heightAnchor.constraint(equalToConstant: 30),
             buttonEdit.widthAnchor.constraint(equalToConstant: 70),
 
-            image.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
-            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            image.heightAnchor.constraint(equalToConstant: 250),
-            image.widthAnchor.constraint(equalToConstant: 250),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
 
-            labelName.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 20),
-            labelName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            labelName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            textFieldName.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            textFieldName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textFieldName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            textFieldName.heightAnchor.constraint(equalToConstant: 52),
+
+            textFieldSurname.topAnchor.constraint(equalTo: textFieldName.bottomAnchor, constant: 20),
+            textFieldSurname.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textFieldSurname.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            textFieldSurname.heightAnchor.constraint(equalToConstant: 52),
+
+            buttonSaved.topAnchor.constraint(equalTo: textFieldSurname.bottomAnchor, constant: 50),
+            buttonSaved.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            buttonSaved.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            buttonSaved.heightAnchor.constraint(equalToConstant: 52)
         ])
+    }
+
+    // MARK: - Func
+
+    private func updateEditingUI(isEditing: Bool) {
+        isEditingProfile = !isEditing
+        buttonSaved.isHidden = !isEditing
+        textFieldName.isUserInteractionEnabled = isEditing
+        textFieldSurname.isUserInteractionEnabled = isEditing
     }
 }
