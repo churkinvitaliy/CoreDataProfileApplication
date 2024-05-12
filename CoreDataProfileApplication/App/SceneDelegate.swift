46aaa6466fac463a45df4dddb6190bd43928c391
Churkin_Vitaliy <ch.vitaliy@telenethd.ru>
2024-05-12 12:47:55 +0900

    Added ModuleBuilder to instantiate UserListViewController

--- CoreDataProfileApplication/App/SceneDelegate.swift
+++ CoreDataProfileApplication/App/SceneDelegate.swift
@@ -7,7 +7,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = UserListViewController()
+        let viewController = ModuleBuilder.createUserListViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
